1ec50a04205f81c62b8a770b61277b92eb460ae8
Konrad Guzek <konrad@guzek.uk>
2024-06-23 23:26:41 +0200

    initial commit

---
+++ settings.lua
@@ -0,0 +1,15 @@
+data:extend({
+    {
+        type = "bool-setting",
+        name = "build-utils-auto-swap-ghost",
+        setting_type = "runtime-per-user",
+        default_value = true,
+    },
+    {
+        type = "bool-setting",
+        name = "build-utils-auto-select-ghost",
+        setting_type = "runtime-per-user",
+        default_value = true,
+    }
+})
+
