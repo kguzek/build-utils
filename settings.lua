ba5d85f22d4ecc2f297c3df76d6f6e76af2afedf
Konrad Guzek <konrad@guzek.uk>
2024-06-23 23:34:27 +0200

    set default for auto-select-ghost to false

--- settings.lua
+++ settings.lua
@@ -1,15 +1,14 @@
 data:extend({
-    {
-        type = "bool-setting",
-        name = "build-utils-auto-swap-ghost",
-        setting_type = "runtime-per-user",
-        default_value = true,
-    },
-    {
-        type = "bool-setting",
-        name = "build-utils-auto-select-ghost",
-        setting_type = "runtime-per-user",
-        default_value = true,
-    }
+  {
+    type = "bool-setting",
+    name = "build-utils-auto-swap-ghost",
+    setting_type = "runtime-per-user",
+    default_value = true,
+  },
+  {
+    type = "bool-setting",
+    name = "build-utils-auto-select-ghost",
+    setting_type = "runtime-per-user",
+    default_value = false,
+  }
 })
-
